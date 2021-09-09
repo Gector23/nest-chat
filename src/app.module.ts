a7af962c3d636b9f1f39ddcfb1ae7a701c500f5a
Vyacheslav Lavrikov <lavrikov2398@gmail.com>
2021-09-09 14:01:19 +0300

    Added TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    }),
+  ],
+})
 export class AppModule {}
